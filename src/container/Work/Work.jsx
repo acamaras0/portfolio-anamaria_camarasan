f13c04b0b1278eb21021293cadccc28f27bae574
Anamaria Camarasan <anamaria.finn0@gmail.com>
2022-12-16 22:56:37 +0200

    work part in progress

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -1,12 +1,117 @@
 import React from "react";
+import { AiFillEye, AiFillGithub } from "react-icons/ai";
+import { motion } from "framer-motion";
+import { AppWrap } from "../../wrapper";
+import { images } from "../../constants";
 import "./Work.scss";
 
 const Work = () => {
-    return (
-        <h1>
-            Work
-        </h1>
-    )
+  const [activeFilter, setActiveFilter] = React.useState("All");
+  const [animateCard, setAnimateCard] = React.useState({ y: 0, opacity: 1 });
+
+  const works = [
+    {
+      title: "Hypertube",
+      description: "Some text here",
+      imgUrl: images.about01,
+      link: "",
+      tags: "",
+    },
+    {
+      title: "Matcha",
+      description: "Some text here",
+      imgUrl: images.about02,
+      link: "",
+      tags: "",
+    },
+    {
+      title: "Camagru",
+      description: "Some text here",
+      imgUrl: images.about03,
+      link: "",
+      tags: "",
+    },
+  ];
+
+  const handleWorkFilter = (item) => {
+    setActiveFilter(item);
+  };
+  return (
+    <>
+      <h2 className="head-text">
+        My <span>Projects</span>
+      </h2>
+      <div className="app__work-filter">
+        {["React", "NodeJS", "PHP", "All"].map((item, index) => (
+          <div
+            key={index}
+            onClick={() => handleWorkFilter(item)}
+            className={`app__work-filter-item app__flex p-text ${
+              activeFilter === item ? "item-active" : ""
+            }`}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+      <motion.div
+        animate={animateCard}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__work-portfolio"
+      >
+        {works.map((work, index) => (
+          <div className="app__work-item app__flex" key={index}>
+            <div className="app__work-img app__flex">
+              <img src={work.imgUrl} alt={work.title} />
+              <motion.div
+                whileHover={{ opacity: [0, 1] }}
+                transition={{
+                  duration: 0.25,
+                  ease: "easeInOut",
+                  staggerChildren: 0.5,
+                }}
+                className="app__work-hover app__flex"
+              >
+                <a href={work.link} target="_blank" rel="noreferrer">
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.9] }}
+                    transition={{
+                      duration: 0.25,
+                    }}
+                    className="app__flex"
+                  >
+                    <AiFillEye />
+                  </motion.div>
+                </a>
+                <a href={work.link} target="_blank" rel="noreferrer">
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.9] }}
+                    transition={{
+                      duration: 0.25,
+                    }}
+                    className="app__flex"
+                  >
+                    <AiFillGithub />
+                  </motion.div>
+                </a>
+              </motion.div>
+            </div>
+            <div className="app__work-content app__flex">
+              <h4>{work.title}</h4>
+              <p className="p-text" style={{ marginTop: 10 }}>
+                {work.description}
+              </p>
+              <div className="app__work-tag app__flex">
+                <p className="p-text">{work.tags}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </motion.div>
+    </>
+  );
 };
 
-export default Work;
+export default AppWrap(Work, "work");
--- src/wrapper/AppWrap.js
+++ src/wrapper/AppWrap.js
@@ -11,8 +11,8 @@ const AppWrap = (Component, idName, classNames) =>
 				<div className="app__wrapper app__flex">
 					<Component />
 					<div className="copyright">
-						<p classname="p-text">@2022 ANAMARIA</p>
-						<p classname="p-text">All rights reserved.</p>
+						<p className="p-text">@2022 ANAMARIA</p>
+						<p className="p-text">All rights reserved.</p>
 					</div>
 				</div>
 				<NavigationDots active={idName} />
