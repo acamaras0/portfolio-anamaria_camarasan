a028a031ea54420a1999a7f58afd990732511452
Anamaria Camarasan <anamaria.finn0@gmail.com>
2022-12-19 22:30:04 +0200

    dohaisuvhdjk

--- src/wrapper/AppWrap.js
+++ src/wrapper/AppWrap.js
@@ -4,19 +4,16 @@ import { NavigationDots, SocialMedia } from "../components";
 //higher order component
 
 const AppWrap = (Component, idName, classNames) =>
-	function HOC() {
-		return (
-			<div id={idName} className={`app__container ${classNames}`}>
-				<SocialMedia />
-				<div className="app__wrapper app__flex">
-					<Component />
-					<div className="copyright">
-						<p className="p-text">@2022 ANAMARIA</p>
-					</div>
-				</div>
-				<NavigationDots active={idName} />
-			</div>
-		);
-	};
+  function HOC() {
+    return (
+      <div id={idName} className={`app__container ${classNames}`}>
+        <SocialMedia />
+        <div className="app__wrapper app__flex">
+          <Component />	
+        </div>
+        <NavigationDots active={idName} />
+      </div>
+    );
+  };
 
 export default AppWrap;
