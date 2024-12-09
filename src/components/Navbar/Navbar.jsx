15d202a3ed456468c236c48aa416cf4cae35877e
Anamaria Camarasan <anamaria.camarasan@wolt.com>
2024-12-09 10:38:04 +0200

    fix navigation

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
 				<img src={images.logo} alt="logo" />
 			</div>
 			<ul className="app__navbar-links">
-				{["home", "about", "skills", "projects", "contact"].map((item) => (
+				{["home", "skills", "projects", "contact"].map((item) => (
 					<li className="app__flex p-text" key={`link-${item}`}>
 						<div />
 						<a href={`#${item}`}>{item}</a>
@@ -30,7 +30,7 @@ const Navbar = () => {
 					>
 						<HiX onClick={() => setToggle(false)} />
 						<ul className="app__navbar-links">
-							{["home", "about", "skills", "projects", "contact"].map(
+							{["home", "skills", "projects", "contact"].map(
 								(item) => (
 									<li
 										className="app__flex p-text"
--- src/components/NavigationDots.jsx
+++ src/components/NavigationDots.jsx
@@ -5,7 +5,7 @@ import React from "react";
 
 const NavigationDots = ({ active }) => (
 	<div className="app__navigation">
-		{["home", "skills", "contact"].map(
+		{["home", "skills", "projects", "contact"].map(
 			(item, index) => (
 				<a
 					href={`#${item}`}
